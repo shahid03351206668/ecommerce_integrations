dfd7abf7d117d93591b6f82ec1d2cb4ec39213ba
Codes Soft <you@example.com>
2025-05-26 15:33:17 +0500

    -

--- ecommerce_integrations/shopify/customer.py
+++ ecommerce_integrations/shopify/customer.py
@@ -6,126 +6,134 @@ from frappe.utils import cstr, validate_phone_number
 
 from ecommerce_integrations.controllers.customer import EcommerceCustomer
 from ecommerce_integrations.shopify.constants import (
-	ADDRESS_ID_FIELD,
-	CUSTOMER_ID_FIELD,
-	MODULE_NAME,
-	SETTING_DOCTYPE,
+    ADDRESS_ID_FIELD,
+    CUSTOMER_ID_FIELD,
+    MODULE_NAME,
+    SETTING_DOCTYPE,
 )
 
 
 class ShopifyCustomer(EcommerceCustomer):
-	def __init__(self, customer_id: str):
-		self.setting = frappe.get_doc(SETTING_DOCTYPE)
-		super().__init__(customer_id, CUSTOMER_ID_FIELD, MODULE_NAME)
+    def __init__(self, customer_id: str):
+        self.setting = frappe.get_doc(SETTING_DOCTYPE)
+        super().__init__(customer_id, CUSTOMER_ID_FIELD, MODULE_NAME)
 
-	def sync_customer(self, customer: Dict[str, Any]) -> None:
-		"""Create Customer in ERPNext using shopify's Customer dict."""
+    def sync_customer(self, customer: Dict[str, Any]) -> None:
+        """Create Customer in ERPNext using shopify's Customer dict."""
 
-		customer_name = cstr(customer.get("first_name")) + " " + cstr(customer.get("last_name"))
-		if len(customer_name.strip()) == 0:
-			customer_name = customer.get("email")
+        customer_name = cstr(customer.get("first_name")) + " " + cstr(customer.get("last_name"))
+        if len(customer_name.strip()) == 0:
+            customer_name = customer.get("email")
 
-		customer_group = self.setting.customer_group
-		super().sync_customer(customer_name, customer_group)
+        customer_group = self.setting.customer_group
+        super().sync_customer(customer_name, customer_group)
 
-		billing_address = customer.get("billing_address", {}) or customer.get("default_address")
-		shipping_address = customer.get("shipping_address", {})
+        billing_address = customer.get("billing_address", {}) or customer.get("default_address")
+        shipping_address = customer.get("shipping_address", {})
 
-		if billing_address:
-			self.create_customer_address(
-				customer_name, billing_address, address_type="Billing", email=customer.get("email")
-			)
-		if shipping_address:
-			self.create_customer_address(
-				customer_name, shipping_address, address_type="Shipping", email=customer.get("email")
-			)
+        if billing_address:
+            self.create_customer_address(
+                customer_name, billing_address, address_type="Billing", email=customer.get("email")
+            )
+        if shipping_address:
+            self.create_customer_address(
+                customer_name,
+                shipping_address,
+                address_type="Shipping",
+                email=customer.get("email"),
+            )
 
-		self.create_customer_contact(customer)
+        self.create_customer_contact(customer)
 
-	def create_customer_address(
-		self,
-		customer_name,
-		shopify_address: Dict[str, Any],
-		address_type: str = "Billing",
-		email: Optional[str] = None,
-	) -> None:
-		"""Create customer address(es) using Customer dict provided by shopify."""
-		address_fields = _map_address_fields(shopify_address, customer_name, address_type, email)
-		super().create_customer_address(address_fields)
+    def create_customer_address(
+        self,
+        customer_name,
+        shopify_address: Dict[str, Any],
+        address_type: str = "Billing",
+        email: Optional[str] = None,
+    ) -> None:
+        """Create customer address(es) using Customer dict provided by shopify."""
+        address_fields = _map_address_fields(shopify_address, customer_name, address_type, email)
+        super().create_customer_address(address_fields)
 
-	def update_existing_addresses(self, customer):
-		billing_address = customer.get("billing_address", {}) or customer.get("default_address")
-		shipping_address = customer.get("shipping_address", {})
+    def update_existing_addresses(self, customer):
+        billing_address = customer.get("billing_address", {}) or customer.get("default_address")
+        shipping_address = customer.get("shipping_address", {})
 
-		customer_name = cstr(customer.get("first_name")) + " " + cstr(customer.get("last_name"))
-		email = customer.get("email")
+        customer_name = cstr(customer.get("first_name")) + " " + cstr(customer.get("last_name"))
+        email = customer.get("email")
 
-		if billing_address:
-			self._update_existing_address(customer_name, billing_address, "Billing", email)
-		if shipping_address:
-			self._update_existing_address(customer_name, shipping_address, "Shipping", email)
+        if billing_address:
+            self._update_existing_address(customer_name, billing_address, "Billing", email)
+        if shipping_address:
+            self._update_existing_address(customer_name, shipping_address, "Shipping", email)
 
-	def _update_existing_address(
-		self,
-		customer_name,
-		shopify_address: Dict[str, Any],
-		address_type: str = "Billing",
-		email: Optional[str] = None,
-	) -> None:
-		old_address = self.get_customer_address_doc(address_type)
+    def _update_existing_address(
+        self,
+        customer_name,
+        shopify_address: Dict[str, Any],
+        address_type: str = "Billing",
+        email: Optional[str] = None,
+    ) -> None:
+        old_address = self.get_customer_address_doc(address_type)
 
-		if not old_address:
-			self.create_customer_address(customer_name, shopify_address, address_type, email)
-		else:
-			exclude_in_update = ["address_title", "address_type"]
-			new_values = _map_address_fields(shopify_address, customer_name, address_type, email)
+        if not old_address:
+            self.create_customer_address(customer_name, shopify_address, address_type, email)
+        else:
+            exclude_in_update = ["address_title", "address_type"]
+            new_values = _map_address_fields(shopify_address, customer_name, address_type, email)
 
-			old_address.update({k: v for k, v in new_values.items() if k not in exclude_in_update})
-			old_address.flags.ignore_mandatory = True
-			old_address.save()
+            for k, v in new_values.items():
+                if k not in exclude_in_update:
+                    setattr(old_address, k, v)
 
-	def create_customer_contact(self, shopify_customer: Dict[str, Any]) -> None:
+            old_address.flags.ignore_mandatory = True
+            old_address.save()
 
-		if not (shopify_customer.get("first_name") and shopify_customer.get("email")):
-			return
+    def create_customer_contact(self, shopify_customer: Dict[str, Any]) -> None:
 
-		contact_fields = {
-			"status": "Passive",
-			"first_name": shopify_customer.get("first_name"),
-			"last_name": shopify_customer.get("last_name"),
-			"unsubscribed": not shopify_customer.get("accepts_marketing"),
-		}
+        if not (shopify_customer.get("first_name") and shopify_customer.get("email")):
+            return
 
-		if shopify_customer.get("email"):
-			contact_fields["email_ids"] = [{"email_id": shopify_customer.get("email"), "is_primary": True}]
+        contact_fields = {
+            "status": "Passive",
+            "first_name": shopify_customer.get("first_name"),
+            "last_name": shopify_customer.get("last_name"),
+            "unsubscribed": not shopify_customer.get("accepts_marketing"),
+        }
 
-		phone_no = shopify_customer.get("phone") or shopify_customer.get("default_address", {}).get(
-			"phone"
-		)
+        if shopify_customer.get("email"):
+            contact_fields["email_ids"] = [
+                {"email_id": shopify_customer.get("email"), "is_primary": True}
+            ]
 
-		if validate_phone_number(phone_no, throw=False):
-			contact_fields["phone_nos"] = [{"phone": phone_no, "is_primary_phone": True}]
+        phone_no = shopify_customer.get("phone") or shopify_customer.get(
+            "default_address", {}
+        ).get("phone")
 
-		super().create_customer_contact(contact_fields)
+        if validate_phone_number(phone_no, throw=False):
+            contact_fields["phone_nos"] = [{"phone": phone_no, "is_primary_phone": True}]
+
+        super().create_customer_contact(contact_fields)
 
 
 def _map_address_fields(shopify_address, customer_name, address_type, email):
-	""" returns dict with shopify address fields mapped to equivalent ERPNext fields"""
-	address_fields = {
-		"address_title": customer_name,
-		"address_type": address_type,
-		ADDRESS_ID_FIELD: shopify_address.get("id"),
-		"address_line1": shopify_address.get("address1") or "Address 1",
-		"address_line2": shopify_address.get("address2"),
-		"city": shopify_address.get("city"),
-		"state": shopify_address.get("province"),
-		"pincode": shopify_address.get("zip"),
-		"country": shopify_address.get("country"),
-		"email_id": email,
-	}
+    """returns dict with shopify address fields mapped to equivalent ERPNext fields"""
+    address_fields = {
+        "address_title": customer_name,
+        "address_type": address_type,
+        ADDRESS_ID_FIELD: shopify_address.get("id"),
+        "address_line1": shopify_address.get("address1") or "Address 1",
+        "address_line2": shopify_address.get("address2"),
+        "city": shopify_address.get("city"),
+        "state": shopify_address.get("province"),
+        "pincode": shopify_address.get("zip"),
+        "country": shopify_address.get("country"),
+        "email_id": email,
+    }
 
-	phone = shopify_address.get("phone")
-	if validate_phone_number(phone, throw=False):
-		address_fields["phone"] = phone
+    phone = shopify_address.get("phone")
+    if validate_phone_number(phone, throw=False):
+        address_fields["phone"] = phone
 
-	return address_fields
+    return address_fields
