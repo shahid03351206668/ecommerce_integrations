5152d5770d421cd763e1076e8b2ed76c56ed4625
Codes Soft <you@example.com>
2025-05-31 21:06:34 +0500

    -

--- ecommerce_integrations/shopify/order.py
+++ ecommerce_integrations/shopify/order.py
@@ -117,6 +117,9 @@ def create_sales_order(shopify_order, setting, company=None):
                 ORDER_ID_FIELD: str(shopify_order.get("id")),
                 ORDER_NUMBER_FIELD: shopify_order.get("name"),
                 "customer": customer,
+                "custom_payment_status": (
+                    "Paid" if shopify_order.get("financial_status") == "paid" else "Pending"
+                ),
                 "transaction_date": getdate(shopify_order.get("created_at")) or nowdate(),
                 "delivery_date": getdate(shopify_order.get("created_at")) or nowdate(),
                 "company": setting.company,
