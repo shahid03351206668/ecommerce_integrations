375ff58b29de3a578ded023c6beffd47c608be43
Codes Soft <you@example.com>
2025-06-26 17:46:24 +0500

    delivery note validation added

--- ecommerce_integrations/hooks.py
+++ ecommerce_integrations/hooks.py
@@ -32,18 +32,18 @@ app_license = "GNU GPL v3.0"
 
 # include js in doctype views
 doctype_js = {
-	"Shopify Settings": "public/js/shopify/old_settings.js",
-	"Sales Order": [
-		"public/js/unicommerce/sales_order.js",
-		"public/js/common/ecommerce_transactions.js",
-	],
-	"Sales Invoice": [
-		"public/js/unicommerce/sales_invoice.js",
-		"public/js/common/ecommerce_transactions.js",
-	],
-	"Item": "public/js/unicommerce/item.js",
-	"Stock Entry": "public/js/unicommerce/stock_entry.js",
-	"Pick List": "public/js/unicommerce/pick_list.js",
+    "Shopify Settings": "public/js/shopify/old_settings.js",
+    "Sales Order": [
+        "public/js/unicommerce/sales_order.js",
+        "public/js/common/ecommerce_transactions.js",
+    ],
+    "Sales Invoice": [
+        "public/js/unicommerce/sales_invoice.js",
+        "public/js/common/ecommerce_transactions.js",
+    ],
+    "Item": "public/js/unicommerce/item.js",
+    "Stock Entry": "public/js/unicommerce/stock_entry.js",
+    "Pick List": "public/js/unicommerce/pick_list.js",
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
@@ -106,60 +106,63 @@ before_uninstall = "ecommerce_integrations.uninstall.before_uninstall"
 # Hook on document methods and events
 
 doc_events = {
-	"Item": {
-		"after_insert": "ecommerce_integrations.shopify.product.upload_erpnext_item",
-		"on_update": "ecommerce_integrations.shopify.product.upload_erpnext_item",
-		"validate": [
-			"ecommerce_integrations.utils.taxation.validate_tax_template",
-			"ecommerce_integrations.unicommerce.product.validate_item",
-		],
-	},
-	"Sales Order": {
-		"on_update_after_submit": "ecommerce_integrations.unicommerce.order.update_shipping_info",
-		"on_cancel": "ecommerce_integrations.unicommerce.status_updater.ignore_pick_list_on_sales_order_cancel",
-	},
-	"Stock Entry": {
-		"validate": "ecommerce_integrations.unicommerce.grn.validate_stock_entry_for_grn",
-		"on_submit": "ecommerce_integrations.unicommerce.grn.upload_grn",
-		"on_cancel": "ecommerce_integrations.unicommerce.grn.prevent_grn_cancel",
-	},
-	"Item Price": {"on_change": "ecommerce_integrations.utils.price_list.discard_item_prices"},
-	"Pick List": {"validate": "ecommerce_integrations.unicommerce.pick_list.validate"},
-	"Sales Invoice": {
-		"on_submit": "ecommerce_integrations.unicommerce.invoice.on_submit",
-		"on_cancel": "ecommerce_integrations.unicommerce.invoice.on_cancel",
-	},
+    "Item": {
+        "after_insert": "ecommerce_integrations.shopify.product.upload_erpnext_item",
+        "on_update": "ecommerce_integrations.shopify.product.upload_erpnext_item",
+        "validate": [
+            "ecommerce_integrations.utils.taxation.validate_tax_template",
+            "ecommerce_integrations.unicommerce.product.validate_item",
+        ],
+    },
+    "Sales Order": {
+        "on_update_after_submit": "ecommerce_integrations.unicommerce.order.update_shipping_info",
+        "on_cancel": "ecommerce_integrations.unicommerce.status_updater.ignore_pick_list_on_sales_order_cancel",
+    },
+    "Delivery Note": {
+        "validate": "ecommerce_integrations.shopify.fulfillment.validate_delivery_note",
+    },
+    "Stock Entry": {
+        "validate": "ecommerce_integrations.unicommerce.grn.validate_stock_entry_for_grn",
+        "on_submit": "ecommerce_integrations.unicommerce.grn.upload_grn",
+        "on_cancel": "ecommerce_integrations.unicommerce.grn.prevent_grn_cancel",
+    },
+    "Item Price": {"on_change": "ecommerce_integrations.utils.price_list.discard_item_prices"},
+    "Pick List": {"validate": "ecommerce_integrations.unicommerce.pick_list.validate"},
+    "Sales Invoice": {
+        "on_submit": "ecommerce_integrations.unicommerce.invoice.on_submit",
+        "on_cancel": "ecommerce_integrations.unicommerce.invoice.on_cancel",
+    },
 }
 
 # Scheduled Tasks
 # ---------------
 
 scheduler_events = {
-	"all": ["ecommerce_integrations.shopify.inventory.update_inventory_on_shopify"],
-	"daily": [],
-	"daily_long": [
-		"ecommerce_integrations.zenoti.doctype.zenoti_settings.zenoti_settings.sync_stocks"
-	],
-	"hourly": [
-		"ecommerce_integrations.shopify.order.sync_old_orders",
-		"ecommerce_integrations.amazon.doctype.amazon_sp_api_settings.amazon_sp_api_settings.schedule_get_order_details",
-	],
-	"hourly_long": [
-		"ecommerce_integrations.zenoti.doctype.zenoti_settings.zenoti_settings.sync_invoices",
-		"ecommerce_integrations.unicommerce.product.upload_new_items",
-		"ecommerce_integrations.unicommerce.status_updater.update_sales_order_status",
-		"ecommerce_integrations.unicommerce.status_updater.update_shipping_package_status",
-	],
-	"weekly": [],
-	"monthly": [],
-	"cron": {
-		# Every five minutes
-		"*/5 * * * *": [
-			"ecommerce_integrations.unicommerce.order.sync_new_orders",
-			"ecommerce_integrations.unicommerce.inventory.update_inventory_on_unicommerce",
-			"ecommerce_integrations.unicommerce.delivery_note.prepare_delivery_note",
-		],
-	},
+    "all": ["ecommerce_integrations.shopify.inventory.update_inventory_on_shopify"],
+    "daily": [],
+    "daily_long": [
+        "ecommerce_integrations.zenoti.doctype.zenoti_settings.zenoti_settings.sync_stocks"
+    ],
+    "hourly": [
+        "ecommerce_integrations.shopify.order.sync_old_orders",
+        "ecommerce_integrations.amazon.doctype.amazon_sp_api_settings.amazon_sp_api_settings.schedule_get_order_details",
+    ],
+    "hourly_long": [
+        "ecommerce_integrations.zenoti.doctype.zenoti_settings.zenoti_settings.sync_invoices",
+        "ecommerce_integrations.unicommerce.product.upload_new_items",
+        "ecommerce_integrations.unicommerce.status_updater.update_sales_order_status",
+        "ecommerce_integrations.unicommerce.status_updater.update_shipping_package_status",
+    ],
+    "weekly": [],
+    "monthly": [],
+    "cron": {
+        # Every five minutes
+        "*/5 * * * *": [
+            "ecommerce_integrations.unicommerce.order.sync_new_orders",
+            "ecommerce_integrations.unicommerce.inventory.update_inventory_on_unicommerce",
+            "ecommerce_integrations.unicommerce.delivery_note.prepare_delivery_note",
+        ],
+    },
 }
 
 
@@ -216,5 +219,5 @@ before_tests = "ecommerce_integrations.utils.before_test.before_tests"
 
 
 default_log_clearing_doctypes = {
-	"Ecommerce Integration Log": 120,
+    "Ecommerce Integration Log": 120,
 }
