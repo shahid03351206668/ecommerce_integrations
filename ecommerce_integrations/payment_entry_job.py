7cd1848089a1b7fe5852a9a8961668f5a6fcaed2
Hussain rizvi <hussainrizvi486@gmail.com>
2025-07-15 19:48:58 +0500

    payment entry account and posting data updated

--- ecommerce_integrations/payment_entry_job.py
+++ ecommerce_integrations/payment_entry_job.py
@@ -4,7 +4,7 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import get_payment_ent
 
 def main():
     unpaid_invoices = frappe.db.sql(
-        f""" 
+    """ 
         SELECT 
             DISTINCT si.name as invoice
         FROM `tabSales Order` so 
@@ -16,12 +16,27 @@ def main():
     """,
         as_dict=True,
     )
-
+    paid_to_account = "1151 - Durchlaufskonto ecom Zahlungen - VA"
+    use_custom = False
+    if frappe.db.exists("Account", paid_to_account):
+        use_custom = True
     for i in unpaid_invoices:
         try:
 
             doc = frappe.get_doc("Sales Invoice", i.get("invoice"))
+            posting_date = None
+            for i in doc.items:
+                if i.sales_order:
+                    posting_date = frappe.db.get_value("Sales Order", i.sales_order, "transaction_date")
+                    break
+
             payment = get_payment_entry(dt="Sales Invoice", dn=doc.name)
+
+            if use_custom:
+                payment.paid_to = paid_to_account
+
+
+            payment.posting_date = posting_date
             payment.reference_no = doc.name
             payment.paid_amount = doc.outstanding_amount
             payment.save()
