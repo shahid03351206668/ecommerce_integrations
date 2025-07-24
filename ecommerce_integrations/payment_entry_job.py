a0afc87b13867570764eaeddbdd27e461e137941
Hussain rizvi <hussainrizvi486@gmail.com>
2025-07-24 15:39:10 +0500

    -

--- ecommerce_integrations/payment_entry_job.py
+++ ecommerce_integrations/payment_entry_job.py
@@ -2,6 +2,52 @@ import frappe
 from erpnext.accounts.doctype.payment_entry.payment_entry import get_payment_entry
 
 
+def run_for_selected_invoices():
+    invoices = [
+        "R-2025-10231",
+        "R-2025-10230",
+        "R-2025-10229",
+        "R-2025-10228",
+        "R-2025-10227",
+        "R-2025-10226",
+    ]
+    paid_to_account = "1151 - Durchlaufskonto ecom Zahlungen - VA"
+    use_custom = False
+
+    if frappe.db.exists("Account", paid_to_account):
+        use_custom = True
+
+    for i in invoices:
+        try:
+            doc = frappe.get_doc("Sales Invoice", i)
+            posting_date = doc.posting_date
+
+            for i in doc.items:
+                if i.sales_order:
+                    posting_date = frappe.db.get_value(
+                        "Sales Order", i.sales_order, "transaction_date"
+                    )
+                    break
+
+            payment = get_payment_entry(dt="Sales Invoice", dn=doc.name)
+
+            if use_custom:
+                payment.paid_to = paid_to_account
+
+            payment.posting_date = posting_date
+            payment.reference_no = doc.name
+            payment.paid_amount = doc.outstanding_amount
+            payment.save()
+            payment.submit()
+        except Exception as e:
+            frappe.log_error(
+                "error while creating payment entry of invoice %s" % i.get("invoice"),
+                str(e),
+            )
+
+    ...
+
+
 def main():
     unpaid_invoices = frappe.db.sql(
         f""" 
@@ -19,7 +65,7 @@ def main():
     )
     paid_to_account = "1151 - Durchlaufskonto ecom Zahlungen - VA"
     use_custom = False
-    
+
     if frappe.db.exists("Account", paid_to_account):
         use_custom = True
 
